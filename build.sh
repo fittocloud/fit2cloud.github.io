1988358b3d682965733bb1039794821ac442a54a
Jason <jason@fit2cloud.com>
2014-07-31 13:26:15 +0800

    change fonts CDN.

--- build.sh
+++ build.sh
@@ -2,22 +2,7 @@
 
 make html
 cp -R build/html/* .
-
-htmls=`ls *.html`
-for html in $htmls ; do
-
-echo $html
-lineId=`grep -n fonts.google $html | awk -F":" '{print $1}'`
-echo $lineId
-if [ "x" == "x$lineId" ]; then
-  echo $html do not need to adjust
-else
-  sed -i "$lineId d" $html
-  sed -i "$lineId i\    <link rel=\"stylesheet\" href=\"_static/fonts.css\" type=\"text/css\" />" $html
-fi
-
-done
-
+sed -i "s/fonts.googleapis.com/fonts.useso.com/g" *.html
 cp _static/cloud.css.template _static/cloud.css
 python makeChinese.py
 #sphinx-build -b pdf ./source build
