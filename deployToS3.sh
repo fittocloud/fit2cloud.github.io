877d7c50e9910ff0707dc426f11997519a37e7fa
Jason <jason@fit2cloud.com>
2014-08-21 15:49:31 +0800

    add exclude file rule to deployToS3.sh

--- deployToS3.sh
+++ deployToS3.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://docs.fit2cloud.com/ --recursive --exclude ".git/*" --exclude "*.sh" --exclude ".gitignore" --exclude ".settings" --exclude ".project" --exclude "build/*"
+aws s3 cp . s3://docs.fit2cloud.com/ --recursive --exclude ".git/*" --exclude "*.sh" --exclude ".gitignore" --exclude ".settings/*" --exclude ".project" --exclude "build/*"
