63219eb1baa598381e1f54a76634c9b66230bb72
Jason <jason@fit2cloud.com>
2014-08-24 11:22:48 +0800

    add meta for SEO.

--- makeChinese.py
+++ makeChinese.py
@@ -9,6 +9,8 @@ cmd="sed -i '' \"s/Previous page/上一页/g\" *.html"
 output=commands.getoutput(cmd)
 cmd="sed -i '' \"s/For 阿里云 1.0//g\" *.html"
 output=commands.getoutput(cmd)
+cmd="sed -i '' \"s/Fit2Cloud 文档/Fit2Cloud在线文档 \| 云管理及DevOps协作平台/g\" *.html"
+output=commands.getoutput(cmd)
 #cmd="sed -i '' \"s/sidebar/目录/g\" *.html"
 #output=commands.getoutput(cmd)
 #print output
