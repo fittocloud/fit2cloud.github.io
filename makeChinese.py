3498cea2c32a6fea2421b433a49676660f85cdd7
Jason <jason@fit2cloud.com>
2014-08-24 10:48:36 +0800

    fix sed failure on mac.

--- makeChinese.py
+++ makeChinese.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 import commands
 
-cmd="sed -i \"s/Page contents/内容/g\" *.html"
+cmd="sed -i '' \"s/Page contents/内容/g\" *.html"
 output=commands.getoutput(cmd)
-cmd="sed -i \"s/Next page/下一页/g\" *.html"
+cmd="sed -i '' \"s/Next page/下一页/g\" *.html"
 output=commands.getoutput(cmd)
-cmd="sed -i \"s/Previous page/上一页/g\" *.html"
+cmd="sed -i '' \"s/Previous page/上一页/g\" *.html"
 output=commands.getoutput(cmd)
-cmd="sed -i \"s/For 阿里云 1.0//g\" *.html"
+cmd="sed -i '' \"s/For 阿里云 1.0//g\" *.html"
 output=commands.getoutput(cmd)
-#cmd="sed -i \"s/sidebar/目录/g\" *.html"
+#cmd="sed -i '' \"s/sidebar/目录/g\" *.html"
 #output=commands.getoutput(cmd)
 #print output
